91f6567bdeadee98a3244c87cea3805e2fe762ee
Mandi Wise <hello@mandiwise.com>
2019-01-07 11:39:13 -0800

    Add missing dependency (due to breaking change).

--- src/init.php
+++ src/init.php
@@ -46,7 +46,7 @@ function inhabitent_blocks_editor_assets() {
 	wp_enqueue_script(
 		'inhabitent-blocks-block-js',
 		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ),
-		array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor' ),
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
 		true 
 	);
